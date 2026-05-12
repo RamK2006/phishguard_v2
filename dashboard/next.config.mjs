96aa9b6ed73adb65819751a8185bba6874de7d42
RamK2006 <itsyash23108@gmail.com>
2026-05-12 12:37:19 +0530

    basic changes to the app

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -2,11 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['img.clerk.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+    ],
   },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1',
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
